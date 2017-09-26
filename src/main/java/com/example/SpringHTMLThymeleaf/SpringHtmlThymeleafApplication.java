1197d011def6f6bfbf5032f65dc482fea597d2a9
Bernard Strong <bstrong71@gmail.com>
2017-09-26 16:28:08 -0400

    initial commit and complete

---
+++ src/main/java/com/example/SpringHTMLThymeleaf/SpringHtmlThymeleafApplication.java
@@ -0,0 +1,12 @@
+package com.example.SpringHTMLThymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHtmlThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHtmlThymeleafApplication.class, args);
+	}
+}
